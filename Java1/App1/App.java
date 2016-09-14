a25df8b38ee51f9aea1994900e05eebc9f501beb
SanderTheDragon <sanderthedragon@zoho.com>
2016-09-14 13:31:20 +0200

    Eh...

---
+++ Java1/App1/App.java
@@ -0,0 +1,9 @@
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("[NAAM]");
+        System.out.println("[STRAAT] [HUISNUMMER]");
+        System.out.println("[STAD]");
+    }
+}
