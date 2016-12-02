c176f46600d9fbf87180e32b97727407d75f5170
SanderTheDragon <sanderthedragon@zoho.com>
2016-12-02 13:41:19 +0100

    Nu ook javadoc voor variables, protected werkt nu ook

--- tools/getset.py
+++ tools/getset.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python
 #coding=utf8
 
-# Copyright © SanderTheDragon (Sander Wolswijk)
-
 import sys
 
-print "Reading file \'" + sys.argv[1] + "\'"
-
-file_ = open(sys.argv[1]).read().splitlines()
-
-lines = []
-nlines = []
-
-i = 0;
-
-prev = False
-
-for line in file_:
-	lines.append(line)
+for arg in sys.argv:
+	print "Reading file \'" + arg + "\'"
 	
-	line = line.strip()
+	file_ = open(arg).read().splitlines()
 	
-	if line.startswith("private") and not "(" in line:
-		varType = line.split(" ")[1];
-		varName = line.split(" ")[2].replace(";", "");
+	lines = []
+	nlines = []
+	
+	i = 0;
+	
+	prev = False
+	
+	for line in file_:
+		lines.append(line)
 		
-		print "Found variable \'" + varName + "\' of type \'" + varType + "\'"
+		line = line.strip()
 		
-		nlines.append("\t")
-		nlines.append("\tpublic " + varType + " get" + varName.title() + "() { return " + varName + "; }")
-		nlines.append("\tpublic void set" + varName.title() + "(" + varType + " new" + varName.title() + ") { " + varName + " = new" + varName.title() + "; }")
-		
-		i = i + 1
-		
-		prev = True
-	else:
-		if prev is True:
-			post = lines[-1]
-			del lines[-1]
+		if line.startswith("private") and not "(" in line:
+			varType = line.split(" ")[1];
+			varName = line.split(" ")[2].replace(";", "");
 			
-			for line_ in nlines:
-				lines.append(line_)
+			print "Found variable \'" + varName + "\' of type \'" + varType + "\'"
 			
-			lines.append(post)
+			nlines.append("\t")
+			nlines.append("\tpublic " + varType + " get" + varName.title() + "() { return " + varName + "; }")
+			nlines.append("\tpublic void set" + varName.title() + "(" + varType + " new" + varName.title() + ") { " + varName + " = new" + varName.title() + "; }")
 			
-			prev = False
-
-file_ = open(sys.argv[1], "w")
-file_.truncate()
-
-for line in lines:
-	file_.write(line + "\n")
-
-print "Wrote " + str(i) + " get/set method pairs"
+			i = i + 1
+			
+			prev = True
+		else:
+			if prev is True:
+				post = lines[-1]
+				del lines[-1]
+				
+				for line_ in nlines:
+					lines.append(line_)
+				
+				lines.append(post)
+				
+				prev = False
+	
+	file_ = open(arg, "w")
+	file_.truncate()
+	
+	for line in lines:
+		file_.write(line + "\n")
+	
+	print "Wrote " + str(i) + " get/set method pairs"
+	
--- tools/javadoc.py
+++ tools/javadoc.py
@@ -5,95 +5,131 @@
 
 import sys
 
-print "Reading file \'" + sys.argv[1] + "\'"
-
-file_ = open(sys.argv[1]).read().splitlines()
-
-className = ""
-
-lines = []
-
-for line in file_:
-	lines.append(line)
+for arg in sys.argv:
+	print "Reading file \'" + arg + "\'"
 	
-	line = line.strip()
+	file_ = open(arg).read().splitlines()
 	
-	if (line.startswith("public") or line.startswith("private")) and not "=" in line:
-		if not "(" in line and not ")" in line:
-			if className is "":
-				post = lines[-1]
-				del lines[-1]
-				
-				lines.append("/**")
-				
-				className = line.split(" ")[2]
-				
-				lines.append(" * @author  FirstName LastName <email@email.com>")
-				lines.append(" * @version 1.0")
-				
-				lines.append(" */")
-				lines.append(post)
+	className = ""
+	
+	lines = []
+	
+	for line in file_:
+		lines.append(line)
+		
+		line = line.strip()
+		
+		if (line.startswith("public") or line.startswith("private") or line.startswith("protected")) and (not ";" in line or "{" in line):
+			if not "(" in line and not ")" in line:
+				if className is "":
+					post = lines[-1]
+					del lines[-1]
+					
+					lines.append("/**")
+					
+					className = line.split(" ")[2]
+					
+					lines.append(" * @author  FirstName LastName <email@email.com>")
+					lines.append(" * @version 1.0")
+					
+					lines.append(" */")
+					lines.append(post)
+			else:
+				if "set" not in line and "get" not in line:
+					post = lines[-1]
+					del lines[-1]
+					
+					lines.append("\t/**")
+					
+					if "{" in line:
+						line = line[0:line.find("{") - 1]
+					
+					parts = line.split(" ")
+					
+					params = []
+					
+					if not "()" in line:
+						params = line.split(", ")
+						params[0] = params[0][params[0].find("(") + 1:]
+						params[-1] = params[-1][:params[-1].find(")")]
+					
+					if parts[1][0:parts[1].find("(")] == className:
+						lines.append("\t * Constructor for \'" + className + "\'")
+						lines.append("\t <p>")
+						lines.append("\t * ...")
+						
+						if params:
+							lines.append("\t * ")
+						
+							for param in params:
+								lines.append("\t * @param " + "{0: <10}".format(param.split(" ")[1]) + " ... (as " + param.split(" ")[0] + ")")
+					else:
+						static = False
+						
+						functionReturn = parts[1]
+						functionName = parts[2][0:parts[2].find("(")]
+						
+						if functionReturn == "static":
+							static = True
+							functionReturn = parts[2]
+							functionName = parts[3][0:parts[3].find("(")]
+						
+						if functionReturn == "void" and functionName == "main" and static == True:
+							lines.append("\t * Main method for \'" + className + "\'")
+							lines.append("\t <p>")
+						
+						lines.append("\t * ...")
+						
+						if params:
+							lines.append("\t * ")
+						
+							for param in params:
+								lines.append("\t * @param " + "{0: <10}".format(param.split(" ")[1]) + " ... (as " + param.split(" ")[0] + ")")
+						
+						if not functionReturn == "void":
+							lines.append("\t * ")
+							lines.append("\t * @return " + "{0: <9}".format("") + " ... (as " + functionReturn + ")")
+					
+					lines.append("\t */")
+					lines.append(post)
 		else:
-			if "set" not in line and "get" not in line:
-				post = lines[-1]
+			if line.startswith("public") or line.startswith("private") or line.startswith("protected"):
+				post = line
 				del lines[-1]
 				
-				lines.append("\t /**")
+				lines.append("\t/**")
 				
-				if "{" in line:
-					line = line[0:line.find("{") - 1]
-				
-				parts = line.split(" ")
-				
-				params = []
-				
-				if not "()" in line:
-					params = line.split(", ")
-					params[0] = params[0][params[0].find("(") + 1:]
-					params[-1] = params[-1][:params[-1].find(")")]
-				
-				if parts[1][0:parts[1].find("(")] == className:
-					lines.append("\t * Constructor for \'" + className + "\'")
-					lines.append("\t * ...")
+				if "=" in line:
+					varValue = line[line.find("=") + 2:line.find("(")]
+					varDecl = line[0:line.find("=") - 1]
 					
-					if params:
-						lines.append("\t * ")
+					parts = varDecl.split(" ")
 					
-						for param in params:
-							lines.append("\t * @param " + "{0: <10}".format(param.split(" ")[1]) + " ...")
+					varName = parts[-1]
+					del parts[-1]
+					
+					varType = " ".join(parts)
+					
+					lines.append("\t * Declares \'" + varName + "\' as \'" + varType + "\' and intializes it as \'" + varValue + "\'")
 				else:
-					static = False
+					varDecl = line[0:line.find(";")]
 					
-					functionReturn = parts[1]
-					functionName = parts[2][0:parts[2].find("(")]
+					parts = varDecl.split(" ")
 					
-					if functionReturn == "static":
-						static = True
-						functionReturn = parts[2]
-						functionName = parts[3][0:parts[3].find("(")]
+					varName = parts[-1]
+					del parts[-1]
 					
-					if functionReturn == "void" and functionName == "main" and static == True:
-						lines.append("\t * Main method for \'" + className + "\'")
+					varType = " ".join(parts)
 					
-					lines.append("\t * ...")
-					
-					if params:
-						lines.append("\t * ")
-					
-						for param in params:
-							lines.append("\t * @param " + "{0: <10}".format(param.split(" ")[1]) + " ...")
-					
-					if not functionReturn == "void":
-						lines.append("\t * ")
-						lines.append("\t * @return " + "{0: <10}".format("") + " ...")
+					lines.append("\t * Declares \'" + varName + "\' as \'" + varType + "\'")
 				
 				lines.append("\t */")
-				lines.append(post)
-
-print "Done"
-
-file_ = open(sys.argv[1], "w")
-file_.truncate()
-
-for line in lines:
-	file_.write(line + "\n")
+				lines.append("\t" + post)
+	
+	print "Done"
+	
+	file_ = open(arg, "w")
+	file_.truncate()
+	
+	for line in lines:
+		file_.write(line + "\n")
